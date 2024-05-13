32161b139f52ff9f122318d9eaed0f131400621f
zachgalbs <106509725+a-hiremath@users.noreply.github.com>
2024-05-13 15:14:38 -0700

    fixed dropdown menu for mobile viewports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger'); 
     const theMenu = document.getElementById('menu'); 
-
+    const navContainer = document.getElementById('navContainer');
 
     function toggleMenu() {
-        theMenu.classList.toggle("hidden");
+        if (theMenu.classList.contains('hidden')) {
+            // Open the menu
+            theMenu.classList.remove('hidden');
+            setTimeout(() => {
+                theMenu.classList.add('opacity-100', 'translate-y-0');
+                theMenu.classList.remove('opacity-0', 'translate-y-[-20px]');
+                navContainer.classList.add('height-auto'); // Adjust height of container
+            }, 1);
+        } else {
+            // Close the menu
+            theMenu.classList.remove('opacity-100', 'translate-y-0');
+            theMenu.classList.add('opacity-0', 'translate-y-[-20px]');
+            setTimeout(() => {
+                theMenu.classList.add('hidden');
+                navContainer.classList.remove('height-auto'); // Reset height of container
+            }, 50); // Match the transition duration
+        }
     }
 
-    hamburger.addEventListener('click', function(){
-        console.log("log");
+    hamburger.addEventListener('click', function() {
         toggleMenu();
-    })
-
-    
-
+    });
 
     function adjustFlower() {
         const flower = document.getElementById("flower") || document.getElementById("unflower");
@@ -21,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     window.addEventListener('resize', adjustFlower);
-    adjustFlower();  // Call on initial load to set correct state
+    adjustFlower();
 
     const transitionLinks = document.querySelectorAll('.page-transition');
     transitionLinks.forEach(function(link) {
@@ -29,7 +41,6 @@ document.addEventListener('DOMContentLoaded', function() {
             e.preventDefault();
             const goTo = this.getAttribute('href');
             if (new URL(goTo, window.location.href).href === new URL(window.location.href).href) {
-                console.log("Already on the same page");
                 document.body.classList.add('animate__animated', 'animate__headShake');
             } else {
                 document.body.classList.add('animate__animated', 'animate__fadeOut');
